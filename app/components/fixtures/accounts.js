128585c28a3630e43b7854f49597199d66c4994f
Jason M Harrison <jason.harrison@q2ebanking.com>
2020-04-03 17:34:46 -0500

    Initial project commit. Swiper.js example stood up - WIP

---
+++ app/components/fixtures/accounts.js
@@ -0,0 +1,48 @@
+const accounts = [
+    {
+        'title': 'Free Checking',
+        'account-number': '1234567890',
+        'balance': {
+            'dollars': '22,716',
+            'cents': '75'
+        },
+        'payment-due': {
+            'dollars': '681',
+            'cents': '00'
+        },
+        'payment-made': {
+            'dollars': '681',
+            'cents': '00'
+        },        
+        'action': {
+            'label': 'Pay Now',
+            'route': '#'
+        },
+        'next-day-date': {
+            'month': 'Feb',
+            'day': '10'
+        },
+        'icon': 'check-edit'
+    },
+    {
+        'title': 'Free Checking',
+        'account-number': '0987654321',
+        'balance': {
+            'dollars': '2,888',
+            'cents': '03'
+        },
+        'payment-due': null,
+        'payment-made': {
+            'dollars': '167',
+            'cents': '56'
+        },        
+        'action': null,
+        'next-day-date': {
+            'month': 'Feb',
+            'day': '10'
+        },
+        'icon': 'check-edit'
+    }
+]
+
+export default accounts;
---
+++ app/components/swiper-component.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import accounts from './fixtures/accounts';
+
+export default Component.extend({
+    accounts: accounts
+});
