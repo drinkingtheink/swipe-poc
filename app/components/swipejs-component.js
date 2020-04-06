d2e04fb42ce86a01f17636659b6d563e98b56ec2
Jason M Harrison <jason.harrison@q2ebanking.com>
2020-04-06 15:10:26 -0500

    Added swipe.js as poc option.

---
+++ app/components/swipejs-component.js
@@ -0,0 +1,24 @@
+import Component from '@ember/component';
+import accounts from './fixtures/accounts';
+import Swipe from 'swipejs';
+
+export default Component.extend({
+    accounts: accounts,
+    swipejs: Swipe,
+    swiperjsInstance: null,
+
+    didInsertElement() {
+        this.swiperjsInstance = new Swipe(document.getElementById('slider'), {
+            startSlide: 0,
+            speed: 400,
+            auto: 0,
+            draggable: true,
+            continuous: true,
+            disableScroll: false,
+            stopPropagation: false,
+            ignore: ".scroller",
+            callback: function(index, elem, dir) {},
+            transitionEnd: function(index, elem) {}
+        });
+    }
+});
