60f6019175c1a1754aefcfcbcd2a52f6f7328995
Jason M Harrison <jason.harrison@q2ebanking.com>
2020-04-06 23:32:52 -0500

    Edit swipejs props in browser.

--- app/components/swipejs-component.js
+++ app/components/swipejs-component.js
@@ -2,23 +2,52 @@ import Component from '@ember/component';
 import accounts from './fixtures/accounts';
 import Swipe from 'swipejs';
 
+const defaultSpeed = 400;
+
 export default Component.extend({
     accounts: accounts,
     swipejs: Swipe,
     swipejsInstance: null,
+    auto: 0,
+    speed: defaultSpeed,
+    continuous: true,
+    componentID: 'swipejs-container',
 
-    didInsertElement() {
-        this.swipejsInstance = new Swipe(document.getElementById('slider'), {
+    actions: {
+        updateSpeed(speed) {
+            let newSpeed = speed.target.value;
+            this.set('speed', newSpeed);
+        },
+        toggleContinuity() {
+            let newCont = !this.continuous;
+            this.set('continuous', newCont);
+        },
+        updateAutoSpeed(speed) {
+            let newSpeed = speed.target.value;
+            this.set('auto', newSpeed);
+        }
+    },
+
+    initSwipe() {
+        this.swipejsInstance = new Swipe(document.getElementById(this.componentID), {
             startSlide: 0,
-            speed: 400,
-            auto: 0,
+            speed: this.speed,
+            auto: this.auto,
             draggable: true,
-            continuous: true,
+            continuous: this.continuous,
             disableScroll: false,
             stopPropagation: false,
             ignore: ".scroller",
             callback: function(index, elem, dir) {},
             transitionEnd: function(index, elem) {}
         });
+    },
+
+    didInsertElement() {
+        this.initSwipe();
+    },
+
+    didRender() {
+        this.initSwipe();
     }
 });
